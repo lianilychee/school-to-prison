c0b28cee77ee972e2a2239bab83b5fa8a6e52247
unknown <bcaporaletti@82WPFV1.olin.edu>
2015-11-28 20:57:27 -0500

    set-up flask for serving current map

--- flask-app/app.py
+++ flask-app/app.py
@@ -1,9 +1,17 @@
-from flask import Flask
+from flask import Flask, send_from_directory
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    return send_from_directory('public/views', 'index.html')
+
+@app.route('scripts/<path:path>')
+def send_js(path):
+    return send_from_directory('public/scripts', path)
+
+@app.route('datasets/<path:path>')
+def send_csv(path):
+	return send_from_directory('public/datasets',path)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
---
+++ flask-app/public/scripts/main.js
@@ -0,0 +1,58 @@
+$(document).ready(function() {
+
+
+    // Click handlers
+    $('#sel-1').click(function() {
+        MAP.update('sel-1');
+    })
+
+    $('#sel-2').click(function() {
+        MAP.update('sel-2');
+    })
+
+    $('#sel-3').click(function() {
+        MAP.update('sel-3');
+    })
+
+    // load CSVs
+    d3.csv("datasets/DisabilityStateGap.csv", function(csv_data) {
+
+        var color = d3.scale.linear()
+            .domain([100, 1000])
+            .range(["white", "black"]);
+
+        var state_fills = {}
+
+        var fillKeys = {}
+
+        for (var i = 0; i < csv_data.length; i++) {
+            fillKeys[csv_data[i]["State"]] = {fillKey: csv_data[i]["State"]}
+            state_fills[csv_data[i]["State"]] = color(parseFloat(csv_data[i]["Diff: B WD vs WOD"]))
+        }
+
+        // draw map of US
+        var map = new Datamap({
+            element: document.getElementById('map'),
+            scope: 'usa',
+            geographyConfig: {
+                popupOnHover: false,
+                highlightOnHover: false
+            },
+            fills: state_fills,
+            data: fillKeys
+        });
+    });
+
+        // if ($('.selector').attr('id') == 'sel-2') {
+        //     console.log('clicked: sel-2')
+        // }
+
+        // if ($('.selector').attr('id') == 'sel-3') {
+        //     console.log('clicked: sel-3')
+        // }
+    
+
+
+
+
+})
