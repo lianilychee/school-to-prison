a42ec7e0d4cff77acbcbd23a3fc9dab0201b7cfe
unknown <bcaporaletti@82WPFV1.olin.edu>
2015-11-28 21:44:09 -0500

    fixed position of maps

--- flask-app/app.py
+++ flask-app/app.py
@@ -5,11 +5,15 @@ app = Flask(__name__)
 def hello_world():
     return send_from_directory('public/views', 'index.html')
 
-@app.route('scripts/<path:path>')
+@app.route('/scripts/<path:path>')
 def send_js(path):
     return send_from_directory('public/scripts', path)
 
-@app.route('datasets/<path:path>')
+@app.route('/style/<path:path>')
+def send_css(path):
+    return send_from_directory('public/style', path)
+
+@app.route('/datasets/<path:path>')
 def send_csv(path):
 	return send_from_directory('public/datasets',path)
 
