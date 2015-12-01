64d0a8bc1e07e74dbbf4d822799ff35b80e82f03
Liani Lye <lye.liani@gmail.com>
2015-12-01 15:37:12 -0500

    cleaned up repo

--- scripts/main.js
+++ scripts/main.js
@@ -17,39 +17,33 @@ $(document).ready(function() {
     // load CSVs
     d3.csv("http://localhost:8000/DisabilityStateGap.csv", function(csv_data) {
 
-        var color = d3.scale.linear()
-            .domain([100, 1000])
-            .range(["white", "black"]);
+        // var color = d3.scale.linear()
+        //     .domain([100, 1000])
+        //     .range(["white", "black"]);
 
-        var state_fills = {}
+        // var state_fills = {}
 
-        var fillKeys = {}
+        // var fillKeys = {}
 
-        for (var i = 0; i < csv_data.length; i++) {
-            fillKeys[csv_data[i]["State"]] = {fillKey: csv_data[i]["State"]}
-            state_fills[csv_data[i]["State"]] = color(parseFloat(csv_data[i]["Diff: B WD vs WOD"]))
-        }
+        // for (var i = 0; i < csv_data.length; i++) {
+        //     fillKeys[csv_data[i]["State"]] = {fillKey: csv_data[i]["State"]}
+        //     state_fills[csv_data[i]["State"]] = color(parseFloat(csv_data[i]["Diff: B WD vs WOD"]))
+        // }
 
-        // draw map of US
-        var map = new Datamap({
-            element: document.getElementById('map'),
-            scope: 'usa',
-            geographyConfig: {
-                popupOnHover: false,
-                highlightOnHover: false
-            },
-            fills: state_fills,
-            data: fillKeys
-        });
+        // // draw map of US
+        // var map = new Datamap({
+        //     element: document.getElementById('map'),
+        //     scope: 'usa',
+        //     geographyConfig: {
+        //         popupOnHover: false,
+        //         highlightOnHover: false
+        //     },
+        //     fills: state_fills,
+        //     data: fillKeys
+        // });
     });
 
-        // if ($('.selector').attr('id') == 'sel-2') {
-        //     console.log('clicked: sel-2')
-        // }
 
-        // if ($('.selector').attr('id') == 'sel-3') {
-        //     console.log('clicked: sel-3')
-        // }
     
 
 
