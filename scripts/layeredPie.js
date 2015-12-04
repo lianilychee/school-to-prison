e374401e22c4ac20e6b3cd6ce4b009b115b99ee2
Liani Lye <lye.liani@gmail.com>
2015-12-03 20:46:23 -0500

    pies now grabbing from data

--- scripts/layeredPie.js
+++ scripts/layeredPie.js
@@ -1,58 +1,79 @@
-function layeredPie(csv_data){
-	data = [
-		{label:"1", pop:1, susp:.3 },
-		{label:"2", pop:2, susp:.1},
-		{label:"3", pop:3, susp:.5},
-		{label:"5", pop:5, susp:.7},
-		{label:"10", pop:10, susp:.2},
-		{label:"1", pop:1, susp:.3}
-	]
-	var radius = 100
-	var color = d3.scale.category10();
-    var color2 = d3.scale.category20();
+function layeredPie(csv_path){
 
-	width = 300
-	height = 300
+    d3.csv(csv_path, function(csv_data) {
+
+        console.log(csv_data);
+
+    	dataset = [
+    		{label:"1", pop:csv_data[49]['Students WD Enrollment'], susp:(csv_data[49]['Suspended Students WD']/csv_data[49]['Students WD Enrollment'])},
+    		{label:"2", pop:csv_data[49]['Students WOD Enrollment'], susp:(csv_data[49]['Suspended Students WOD']/csv_data[49]['Students WOD Enrollment'])},
+            // {label:"2", pop:csv_data[50]['Students WOD Enrollment'], susp:0.4},
+    		// {label:"3", pop:3, susp:0.5},
+    		// {label:"5", pop:5, susp:0.7},
+    		// {label:"10", pop:10, susp:0.2},
+    		// {label:"1", pop:1, susp:0.3}
+    	]
+
+    	var radius = 100
+    	var color = d3.scale.category10();
+        var color2 = "white";
+
+        // dimensions of the svg
+    	width = 300
+    	height = 300
+
+        // stick an SVG to the body of index.html
+    	var svg = d3.select("body").append("svg")
+          .attr("width", width)
+          .attr("height", height)
+          .append("g")
+          .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
+
+        // initialize pie chart
+        var pie = d3.layout.pie()
+            .sort(null)
+            .value(function(d) { return d.pop; });
+
+        // initialize the outer slice
+        var arc = d3.svg.arc()
+            .outerRadius(radius)
+            .innerRadius(0)
+
+        // initialize the inner slice
+        var arc2 = d3.svg.arc()
+            .outerRadius(function(d){
+                // console.log(d.data.susp);
+                return radius * d.data.susp;
+            })
+            .innerRadius(0)
+
+        // initialize labels
+        var labelArc = d3.svg.arc()
+            .outerRadius(radius + 100)
+            .innerRadius(0)
 
 
-	var svg = d3.select("body").append("svg")
-      .attr("width", width)
-      .attr("height", height)
-    .append("g")
-      .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
+        // DRAW ALL THE OBJECTS
 
-    var pie = d3.layout.pie()
-        .sort(null)
-        .value(function(d) { return d.pop; });
-    var arc = d3.svg.arc()
-        .outerRadius(radius)
-        .innerRadius(0)
-    var arc2 = d3.svg.arc()
-        .outerRadius(function(d){
-            console.log(d);
-            return radius * d.data.susp;
-        })
-        .innerRadius(0)
-    var labelArc = d3.svg.arc()
-        .outerRadius(radius + 100)
-        .innerRadius(0)
+    	var g = svg.selectAll(".arc")
+            .data(pie(dataset))
+            .enter()
+            .append("g")
+            .attr("class", "arc");
 
-	var g = svg.selectAll(".arc")
-        .data(pie(data))
-        .enter()
-        .append("g")
-        .attr("class", "arc");
 
-    g.append("path")
-        .attr("d", arc)
-        .style("fill", function(d,i) { return color(i); });
+        g.append("path")
+            .attr("d", arc)
+            .style("fill", function(d,i) { return color(i); });
 
-    g.append("path")
-        .attr("d", arc2)
-        .style("fill", function(d,i) { return color2(i); });
 
-    g.append("text")
-        .attr("transform", function(d) { return "translate(" + labelArc.centroid(d) + ")"; })
-        .attr("dy", ".35em")
-        .text(function(d,i) { return d.data.label; });
+        g.append("path")
+            .attr("d", arc2)
+            .style("fill", function(d,i) { return color2(i); });
+
+        g.append("text")
+            .attr("transform", function(d) { return "translate(" + labelArc.centroid(d) + ")"; })
+            .attr("dy", ".35em")
+            .text(function(d,i) { return d.data.label; });
+    })
 }
--- scripts/main.js
+++ scripts/main.js
@@ -1,34 +1,38 @@
 $(document).ready(function() {
 
-    layeredPie([]);
+    layeredPie("http://localhost:8000/SuspensionByStateAndTotal.csv");
+   
+
+// EXAMPLE
+
     // load CSVs
-    //d3.csv("http://localhost:8000/SuspensionByStateAndTotal.csv", function(csv_data) {
+    // d3.csv("http://localhost:8000/SuspensionByStateAndTotal.csv", function(csv_data) {
 
-        // var color = d3.scale.linear()
-        //     .domain([100, 1000])
-        //     .range(["white", "black"]);
+    //     var color = d3.scale.linear()
+    //         .domain([100, 1000])
+    //         .range(["white", "black"]);
 
-        // var state_fills = {}
+    //     var state_fills = {}
 
-        // var fillKeys = {}
+    //     var fillKeys = {}
 
-        // for (var i = 0; i < csv_data.length; i++) {
-        //     fillKeys[csv_data[i]["State"]] = {fillKey: csv_data[i]["State"]}
-        //     state_fills[csv_data[i]["State"]] = color(parseFloat(csv_data[i]["Diff: B WD vs WOD"]))
-        // }
+    //     for (var i = 0; i < csv_data.length; i++) {
+    //         fillKeys[csv_data[i]["State"]] = {fillKey: csv_data[i]["State"]}
+    //         state_fills[csv_data[i]["State"]] = color(parseFloat(csv_data[i]["Diff: B WD vs WOD"]))
+    //     }
 
-        // // draw map of US
-        // var map = new Datamap({
-        //     element: document.getElementById('map'),
-        //     scope: 'usa',
-        //     geographyConfig: {
-        //         popupOnHover: false,
-        //         highlightOnHover: false
-        //     },
-        //     fills: state_fills,
-        //     data: fillKeys
-        // });
-    //});
+    //     // draw map of US
+    //     var map = new Datamap({
+    //         element: document.getElementById('map'),
+    //         scope: 'usa',
+    //         geographyConfig: {
+    //             popupOnHover: false,
+    //             highlightOnHover: false
+    //         },
+    //         fills: state_fills,
+    //         data: fillKeys
+    //     });
+    // });
 
 
     
