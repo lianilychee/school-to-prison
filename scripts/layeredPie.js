3c59d8e8883e50169f86acb1f0c58c6c9a8c171e
Liani Lye <lye.liani@gmail.com>
2015-12-06 20:28:08 -0500

    added regional bits

--- scripts/layeredPie.js
+++ scripts/layeredPie.js
@@ -1,82 +1,77 @@
 var disability = ""
 var race = ""
 
-function layeredPie(csv_path){
+function layeredPie(csv_data){
 
-    d3.csv(csv_path, function(csv_data) {
+	dataset = [
+		{label:"WD", pop:csv_data[49]['Students WD Enrollment'], susp:(csv_data[49]['Suspended Students WD']/csv_data[49]['Students WD Enrollment']), selected:false},
+		{label:"WOD", pop:csv_data[49]['Students WOD Enrollment'], susp:(csv_data[49]['Suspended Students WOD']/csv_data[49]['Students WOD Enrollment']), selected:false},
+	]
+    
+	var radius = 100
+	var color = d3.scale.category10();
+    var color2 = "white";
 
-        // console.log(csv_data);
+    // dimensions of the svg
+	width = 300
+	height = 300
 
-    	dataset = [
-    		{label:"WD", pop:csv_data[49]['Students WD Enrollment'], susp:(csv_data[49]['Suspended Students WD']/csv_data[49]['Students WD Enrollment']), selected:false},
-    		{label:"WOD", pop:csv_data[49]['Students WOD Enrollment'], susp:(csv_data[49]['Suspended Students WOD']/csv_data[49]['Students WOD Enrollment']), selected:false},
-    	]
+    // stick an SVG to the body of index.html
+	var svg = d3.select("#pie").append("svg")
+      .attr("width", width)
+      .attr("height", height)
+      .append("g")
+      .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
 
-    	var radius = 100
-    	var color = d3.scale.category10();
-        var color2 = "white";
+    // initialize pie chart
+    var pie = d3.layout.pie()
+        .sort(null)
+        .value(function(d) { return d.pop; });
 
-        // dimensions of the svg
-    	width = 300
-    	height = 300
+    // initialize the outer slice
+    var arc = d3.svg.arc()
+        .outerRadius(radius)
+        .innerRadius(0)
 
-        // stick an SVG to the body of index.html
-    	var svg = d3.select("#pie").append("svg")
-          .attr("width", width)
-          .attr("height", height)
-          .append("g")
-          .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
+    // initialize the inner slice
+    var arc2 = d3.svg.arc()
+        .outerRadius(function(d){
+            return radius * d.data.susp;
+        })
+        .innerRadius(0)
 
-        // initialize pie chart
-        var pie = d3.layout.pie()
-            .sort(null)
-            .value(function(d) { return d.pop; });
-
-        // initialize the outer slice
-        var arc = d3.svg.arc()
-            .outerRadius(radius)
-            .innerRadius(0)
-
-        // initialize the inner slice
-        var arc2 = d3.svg.arc()
-            .outerRadius(function(d){
-                return radius * d.data.susp;
-            })
-            .innerRadius(0)
-
-        // initialize labels
-        var labelArc = d3.svg.arc()
-            .outerRadius(radius + 100)
-            .innerRadius(0)
+    // initialize labels
+    var labelArc = d3.svg.arc()
+        .outerRadius(radius + 100)
+        .innerRadius(0)
 
 
-        // DRAW ALL THE OBJECTS
-        function update(){
-            svg.selectAll("g").remove()
-        	var g = svg.selectAll(".arc")
-                .data(pie(dataset))
-                .enter()
-                .append("g")
-                .attr("class", "arc")
-                .on("click", function(d,i) {
-                    dataset[i]["selected"] = !dataset[i]["selected"];
-                    update();
-                });
+    // DRAW ALL THE OBJECTS
+    function update(){
+        svg.selectAll("g").remove()
+    	var g = svg.selectAll(".arc")
+            .data(pie(dataset))
+            .enter()
+            .append("g")
+            .attr("class", "arc")
+            .on("click", function(d,i) {
+                dataset[i]["selected"] = !dataset[i]["selected"];
+                update();
+            });
 
-            g.append("path")
-                .attr("d", arc)
-                .style("fill", function(d,i) { return d.data.selected ? d3.rgb(color(i)).darker(2) : color(i); });
+        g.append("path")
+            .attr("d", arc)
+            .style("fill", function(d,i) { return d.data.selected ? d3.rgb(color(i)).darker(2) : color(i); });
 
 
-            g.append("path")
-                .attr("d", arc2)
-                .style("fill", function(d,i) { return d.data.selected ? color(i) : d3.rgb(color(i)).brighter(2); });
+        g.append("path")
+            .attr("d", arc2)
+            .style("fill", function(d,i) { return d.data.selected ? color(i) : d3.rgb(color(i)).brighter(2); });
 
-            g.append("text")
-                .attr("transform", function(d) { return "translate(" + labelArc.centroid(d) + ")"; })
-                .attr("dy", ".35em")
-                .text(function(d,i) { return d.data.label; });
-        }
-        update();
-    })
+        g.append("text")
+            .attr("transform", function(d) { return "translate(" + labelArc.centroid(d) + ")"; })
+            .attr("dy", ".35em")
+            .text(function(d,i) { return d.data.label; });
+    }
+    update();
 }
--- scripts/main.js
+++ scripts/main.js
@@ -1,18 +1,28 @@
 $(document).ready(function() {
 
-    layeredPie("http://localhost:8000/SuspensionByStateAndTotal.csv");
+    // load CSV data
+    d3.csv("http://localhost:8000/SuspensionByStateAndTotal.csv", function(csv_data) {
+
+        d3.csv("http://localhost:8000/Are_We_Closing_Secondary_regions.csv", function(regions_data) {
+
+            layeredPie(csv_data);
+            regions(regions_data, csv_data);
+
+        })
+    })
+
  
     percentage = 0.5
 
     var containerHeight = $('#person-image').height();
 
-    console.log(containerHeight)
+    // console.log(containerHeight)
 
     var svgContainer = d3.select('#person').append('svg')
                                                 .attr('width', '100%')
                                                 .attr('height', '100%')
 
-    console.log(percentage)
+    // console.log(percentage)
 
     var personFill = svgContainer.append('rect')
                                .attr('x', 0)
