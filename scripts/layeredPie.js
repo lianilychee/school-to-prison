3e0280489f59797b42acdef2411a978342e4021b
jlangowitz <jlangowitz@gmail.com>
2015-12-08 01:09:16 -0500

    swoosh and unswoosh

--- scripts/layeredPie.js
+++ scripts/layeredPie.js
@@ -9,7 +9,7 @@ var WODcolor = "#00DFDD"
     Each arc contains a label, population, suspension rate, color, and selection Y/N. **/
 function buildDataset(csv_data, row_number, pie_state){
     var dataset = []
-    if (pie_state == "default" || pie_state == "WODRace"){
+    if (pie_state == "default" || pie_state == "WOD"){
         dataset.push(
             {label:"WD",
             pop:csv_data[row_number]['Students WD Enrollment'],
@@ -17,7 +17,7 @@ function buildDataset(csv_data, row_number, pie_state){
             color:WDcolor,
             selected:false});
     }
-    if (pie_state == "WDRace"){
+    if (pie_state == "WD"){
         for (var i = 0; i < raceColumns.length; i++){
             dataset.push(
                 {label:raceColumns[i] + "s WD",
@@ -27,7 +27,7 @@ function buildDataset(csv_data, row_number, pie_state){
                 selected:false});
         }
     }
-    if (pie_state == "default" || pie_state == "WDRace"){
+    if (pie_state == "default" || pie_state == "WD"){
         dataset.push(
             {label:"WOD",
             pop:csv_data[row_number]['Students WOD Enrollment'],
@@ -35,7 +35,7 @@ function buildDataset(csv_data, row_number, pie_state){
             color:WODcolor,
             selected:false});
     }
-    if (pie_state == "WODRace"){
+    if (pie_state == "WOD"){
         for (var i = 0; i < raceColumns.length; i++){
             dataset.push(
                 {label:raceColumns[i] + "s WOD",
@@ -70,6 +70,14 @@ function layeredPie(csv_data){
         .style("font-family","sans-serif")
         .style("font-size","20px");
         
+    var back_button = svg.append("foreignObject")
+        .attr("transform", "translate(" + 17 * width / 20 + "," + height / 15 + ")")
+        // .style("font-family","sans-serif")
+        // .style("font-size","20px")
+        // .text("Back")
+        .style("visibility", "hidden")
+        .append("xhtml:body")
+            .html("<button class=\"back-button\">Back</button>");
 
     var infog = svg.append("g")
         .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")")
@@ -121,7 +129,7 @@ function layeredPie(csv_data){
             .on("click", function(d,i) {
 
                 if(d.data.label == "WD" || d.data.label == "WOD"){
-                    pie_state = d.data.label+"Race";
+                    pie_state = d.data.label;
                     update(csv_data);
                 } else {
                     d.data.selected = !d.data.selected;
@@ -143,24 +151,73 @@ function layeredPie(csv_data){
         g.append("path")
             .attr("d", arc)
             .attr("class","whole_arc")
-            .style("fill", function(d,i) { return d.data.color; });
+            .style("fill", function(d,i) { return d.data.color; })
+            .each(function(d) { this._current = d });
 
 
         g.append("path")
             .attr("d", susp_arc)
             .attr("class","susp_arc")
-            .style("fill", function(d,i) { return d3.rgb(d.data.color).brighter(1); });
+            .style("fill", function(d,i) { return d3.rgb(d.data.color).brighter(1); })
+            .each(function(d) { this._current = d });
+
+        if (pie_state != "default") {
+            var pieSwoosh = d3.layout.pie()
+                .sort(null)
+                .value(function(d) {
+                    // console.log(d.label, pie_state);
+                    // console.log(d.label.indexOf(pie_state));
+                    return d.label.indexOf(pie_state) < 0 ? 0 : d.pop;
+                });
+            console.log(g.data());
+            g = g.data(pieSwoosh(dataset));
+            console.log(g.data());
+            g.selectAll(".whole_arc").transition().duration(750).attrTween("d", arcTween);
+            g.selectAll(".susp_arc").transition().duration(750).attrTween("d", arcTween);
+            back_button.style("visibility", "visible");
+
+            back_button.on("click", function() {
+                g = g.data(pie(dataset));
+                g.selectAll(".whole_arc").transition().duration(750).attrTween("d", arcTween);
+                g.selectAll(".susp_arc").transition().duration(750).attrTween("d", arcTween);
+                pie_state = "default";
+                setTimeout(function() { update(csv_data); }, 750);
+                // update(csv_data);
+            });
+        } else {
+            back_button.style("visibility", "hidden");
+        };
+    }
+
+    function arcTween(d, i, a) {
+        var i = d3.interpolate(this._current, d3.select(this.parentNode).datum());
+        console.log(d);
+        console.log(this._current);
+        console.log(i(0));
+        console.log(i(1));
+        this._current = i(0);
+        if (d3.select(this).attr("class") == "whole_arc") {
+            return function(t) {
+                // console.log(t);
+                // console.log(arc(i(t)));
+                return arc(i(t));
+            };
+        } else {
+            return function(t) {
+                return susp_arc(i(t));
+            };
+        };
     }
 
     function setInfog(d) {
-        infog.style("opacity",1);
+        infog.style("visibility", "visible");
         info_text.line1.text((d.data.susp*100).toFixed(2) + "% of");
         info_text.line2.text(d.data.label);
         info_text.line3.text("Suspended");
     }
 
     function hideInfog() {
-        infog.style("opacity",0);
+        infog.style("visibility", "hidden");
     }
  
     update(csv_data);
